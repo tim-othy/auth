b4ac4a95a9338629b3625f1f241b1a69e00afcb0
Timothy Mehay <timothymehay@gmail.com>
2020-05-30 19:07:25 +0200

    Added basic spring-boot auth-client using custom auth-server

---
+++ src/test/java/com/spring/auth/AuthClient/AuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.auth.AuthClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
